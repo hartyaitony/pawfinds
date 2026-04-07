1a20f4bb3a32ac6a3b6b38dce9c68828888ff096
hartyaitony <hartyaitony@gmail.com>
2026-04-07 17:08:35 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,35 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3457;
+const DIR = __dirname;
+
+const MIME = {
+  '.html': 'text/html; charset=utf-8',
+  '.css': 'text/css; charset=utf-8',
+  '.js': 'application/javascript; charset=utf-8',
+  '.json': 'application/json; charset=utf-8',
+  '.csv': 'text/csv; charset=utf-8',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.svg': 'image/svg+xml',
+  '.ico': 'image/x-icon',
+};
+
+http.createServer((req, res) => {
+  let filePath = path.join(DIR, req.url === '/' ? 'index.html' : req.url.split('?')[0]);
+  const ext = path.extname(filePath).toLowerCase();
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end('Not found');
+      return;
+    }
+    res.writeHead(200, {
+      'Content-Type': MIME[ext] || 'application/octet-stream',
+      'Access-Control-Allow-Origin': '*',
+    });
+    res.end(data);
+  });
+}).listen(PORT, () => console.log(`PawFinds UpGrade running on http://localhost:${PORT}`));
